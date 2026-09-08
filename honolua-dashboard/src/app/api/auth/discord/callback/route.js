edcadcaad43c7699796afc0a9e2207a01f37d6e5
CamoBlamo <camolid93@gmail.com>
2026-09-08 18:42:28 -0400

    remove url from end see if it works

--- honolua-dashboard/src/app/api/auth/discord/callback/route.js
+++ honolua-dashboard/src/app/api/auth/discord/callback/route.js
@@ -56,7 +56,7 @@ export async function GET(request) {
     client_secret: process.env.DISCORD_CLIENT_SECRET,
     grant_type: "authorization_code",
     code,
-    redirect_uri: `${process.env.PUBLIC_URL}/api/auth/discord/callback`,
+    redirect_uri: `${process.env.PUBLIC_URL}`,
   }),
 });
 if (!tokenRes.ok) {
